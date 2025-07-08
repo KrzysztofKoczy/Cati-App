5ceace16017afac73ebd963571f97ec2e7a23cfd
Krzysztof <krzysztof.koczy99@gmail.com>
2025-07-08 08:14:13 +0200

    add infinite-scroll directive

--- src/app/directives/infinite-scroll.directive.ts
+++ src/app/directives/infinite-scroll.directive.ts
@@ -1,10 +1,38 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, inject, OnDestroy, OnInit, output } from "@angular/core"
 
 @Directive({
-  selector: '[appInfiniteScroll]'
+  selector: "[appInfiniteScroll]",
+  standalone: true,
 })
-export class InfiniteScrollDirective {
+export class InfiniteScrollDirective implements OnInit, OnDestroy {
+  private element = inject(ElementRef);
 
-  constructor() { }
+  scrolled = output<void>();
 
+  private observer!: IntersectionObserver;
+
+  ngOnInit() {
+    // TODO change rootMargin
+    const options = {
+      root: null,
+      rootMargin: "50px",
+      threshold: 0.1,
+    }
+
+    this.observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          this.scrolled.emit();
+        }
+      })
+    }, options);
+
+    this.observer.observe(this.element.nativeElement);
+  }
+
+  ngOnDestroy() {
+    if (this.observer) {
+      this.observer.disconnect();
+    }
+  }
 }
