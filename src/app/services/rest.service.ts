e4f90f7394a5fd7541a9b49f77f9bbd130cda76e
Krzysztof <krzysztof.koczy99@gmail.com>
2025-07-08 08:15:20 +0200

    add rest service

--- src/app/services/rest.service.ts
+++ src/app/services/rest.service.ts
@@ -1,9 +1,20 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+
+// TODO add to model
+interface CatFactResponse {
+  data: string[]
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class RestService {
+  private httpClient = inject(HttpClient);
 
   constructor() { }
+
+  getCatFacts(): Promise<CatFactResponse | undefined> {
+    return this.httpClient.get<CatFactResponse>('https://meowfacts.herokuapp.com/').toPromise();
+  }
 }
