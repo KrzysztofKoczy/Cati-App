87197b89d0f1e5a4418f3afdc9c11f3cfa90cc8a
Krzysztof <krzysztof.koczy99@gmail.com>
2025-07-08 17:40:19 +0200

    develop card component

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,12 +1,21 @@
-import { Component, inject, input, Input } from "@angular/core"
+import { ChangeDetectionStrategy, Component, inject, input, Input } from "@angular/core"
 import { CommonModule } from "@angular/common"
+import { FavoritesService } from "../../services/favorities.service"
+import { IconComponent } from "../icon/icon.component";
 
 @Component({
-  selector: "app-cat-fact-card",
-  imports: [CommonModule],
+  selector: "app-card",
+  imports: [CommonModule, IconComponent],
   templateUrl: "./card.component.html",
   styleUrls: ["./card.component.scss"],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class CatFactCardComponent {
+export class CardComponent {
+  favoritesService = inject(FavoritesService);
+
   fact = input.required<string>();
+
+  toggleFavorite(): void {
+    this.favoritesService.toggleFavorite(this.fact());
+  }
 }
