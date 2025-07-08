ff0d95a590135febed30ccce9c850fb828ff957b
Krzysztof <krzysztof.koczy99@gmail.com>
2025-07-08 07:38:13 +0200

    add navigation

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, inject, output, input } from "@angular/core"
+import { CommonModule } from "@angular/common"
+import { FavoritesService } from "../../services/favorities.service"
+
+export type TabType = "facts" | "favorites"
 
 @Component({
-  selector: 'app-navigation',
-  imports: [],
-  templateUrl: './navigation.component.html',
-  styleUrl: './navigation.component.scss'
+  selector: "app-navigation",
+  imports: [CommonModule],
+  templateUrl: "./navigation.component.html",
+  styleUrls: ["./navigation.component.scss"],
 })
 export class NavigationComponent {
+  favoritesService = inject(FavoritesService);
 
+  activeTab = input<TabType>("facts");
+  factsCount = input<number>(0);
+
+  tabChange = output<TabType>();
+  logout = output<void>();
+
+  switchTab(tab: TabType): void {
+    if (tab !== this.activeTab()) {
+      this.tabChange.emit(tab);
+    }
+  }
+
+  onLogout(): void {
+    this.logout.emit();
+  }
 }
