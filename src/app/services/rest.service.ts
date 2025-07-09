aa94ba08959df4674a38305b611f9a1f8d4f0d72
Krzysztof <krzysztof.koczy99@gmail.com>
2025-07-09 17:36:20 +0200

    Code Refactor + ReadMe

--- src/app/services/rest.service.ts
+++ src/app/services/rest.service.ts
@@ -1,11 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-
-// TODO add to model
-interface CatFactResponse {
-  data: string[]
-}
+import { CatFactResponse } from '../model/interface';
 
 @Injectable({
   providedIn: 'root'
