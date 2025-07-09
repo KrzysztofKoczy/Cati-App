9a16acaa339438860d7888bc538183f2d88e76f0
Krzysztof <krzysztof.koczy99@gmail.com>
2025-07-09 13:52:12 +0200

    add dashboard tests

--- src/app/components/dashboard/dashboard.component.spec.ts
+++ src/app/components/dashboard/dashboard.component.spec.ts
@@ -1,23 +1,78 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { DashboardComponent } from "./dashboard.component";
+import { CatFactsService } from "../../services/cat-facts.service";
+import { NavigationService } from "../../services/navigation.service";
+import { AuthenticationService } from "../../services/authentication.service";
+import { of } from "rxjs";
 
-import { DashboardComponent } from './dashboard.component';
-
-describe('DashboardComponent', () => {
+describe("DashboardComponent", () => {
   let component: DashboardComponent;
   let fixture: ComponentFixture<DashboardComponent>;
+  let catFactsServiceSpy: jasmine.SpyObj<CatFactsService>;
+  let navigationServiceSpy: jasmine.SpyObj<NavigationService>;
+  let authenticationServiceSpy: jasmine.SpyObj<AuthenticationService>;
 
   beforeEach(async () => {
+    catFactsServiceSpy = jasmine.createSpyObj("CatFactsService", ["loadCatFacts"]);
+    navigationServiceSpy = jasmine.createSpyObj("NavigationService", ["setFactsCount"]);
+    authenticationServiceSpy = jasmine.createSpyObj("AuthenticationService", ["logout"]);
+
     await TestBed.configureTestingModule({
-      imports: [DashboardComponent]
-    })
-    .compileComponents();
+      imports: [DashboardComponent],
+      providers: [
+        { provide: CatFactsService, useValue: catFactsServiceSpy },
+        { provide: NavigationService, useValue: navigationServiceSpy },
+        { provide: AuthenticationService, useValue: authenticationServiceSpy },
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(DashboardComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
+  });
+
+  it("should initialize with empty facts array", () => {
+    expect(component.catFacts()).toEqual([]);
+  });
+
+  it("should load initial facts on ngOnInit and update facts and navigation count", () => {
+    const mockFacts = ["Fact 1", "Fact 2"];
+
+    catFactsServiceSpy.loadCatFacts.and.returnValue(of(mockFacts));
+
+    component.ngOnInit();
+
+    expect(catFactsServiceSpy.loadCatFacts).toHaveBeenCalled();
+
+    setTimeout(() => {
+      expect(component.catFacts()).toEqual(mockFacts);
+      expect(navigationServiceSpy.setFactsCount).toHaveBeenCalledWith(mockFacts.length);
+    }, 0);
+  });
+
+  it("should append more facts on loadMoreFacts and update navigation count", () => {
+    const initialFacts = ["Fact 1"];
+    const newFacts = ["Fact 2", "Fact 3"];
+
+    component.catFacts.set(initialFacts);
+    catFactsServiceSpy.loadCatFacts.and.returnValue(of(newFacts));
+
+    component.loadMoreFacts();
+
+    setTimeout(() => {
+      expect(component.catFacts()).toEqual([...initialFacts, ...newFacts]);
+      expect(navigationServiceSpy.setFactsCount).toHaveBeenCalledWith(3);
+    }, 0);
+  });
+
+  it("should clean up subscriptions on ngOnDestroy", () => {
+    spyOn(component['destroy$'], 'next');
+
+    component.ngOnDestroy();
+    
+    expect(component['destroy$'].next).toHaveBeenCalled();
   });
 });
