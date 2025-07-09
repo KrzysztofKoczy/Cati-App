546311d2f58458dac0bcebc63b03a45de5883fea
Krzysztof <krzysztof.koczy99@gmail.com>
2025-07-09 13:57:18 +0200

    add card tests

--- src/app/components/card/card.component.spec.ts
+++ src/app/components/card/card.component.spec.ts
@@ -1,23 +1,67 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { CardComponent } from "./card.component";
+import { FavoritesService } from "../../services/favorites.service";
+import { IconComponent } from "../icon/icon.component";
 
-import { CardComponent } from './card.component';
-
-describe('CardComponent', () => {
+describe("CardComponent", () => {
   let component: CardComponent;
   let fixture: ComponentFixture<CardComponent>;
+  let favoritesServiceSpy: jasmine.SpyObj<FavoritesService>;
 
   beforeEach(async () => {
+    favoritesServiceSpy = jasmine.createSpyObj("FavoritesService", [
+      "isFavorite",
+      "toggleFavorite"
+    ]);
+
     await TestBed.configureTestingModule({
-      imports: [CardComponent]
-    })
-    .compileComponents();
+      imports: [CardComponent, IconComponent],
+      providers: [
+        { provide: FavoritesService, useValue: favoritesServiceSpy }
+      ]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(CardComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
+  });
+
+  it("should display the provided fact", () => {
+    const testFact = "Cats have 32 muscles in each ear.";
+
+    fixture.componentRef.setInput('fact', testFact);
+    favoritesServiceSpy.isFavorite.and.returnValue(false);
+    fixture.detectChanges();
+
+    const factText = fixture.nativeElement.querySelector(".card-text");
+
+    expect(factText.textContent).toContain(testFact);
+  });
+
+  it("should call toggleFavorite on button click", () => {
+    const testFact = "Cats purr at 26 cycles per second.";
+
+    fixture.componentRef.setInput('fact', testFact);
+    favoritesServiceSpy.isFavorite.and.returnValue(false);
+    fixture.detectChanges();
+
+    const button = fixture.nativeElement.querySelector(".favorite-btn");
+
+    button.click();
+
+    expect(favoritesServiceSpy.toggleFavorite).toHaveBeenCalledWith(testFact);
+  });
+
+  it("should add 'favorited' class when fact is favorited", () => {
+    fixture.componentRef.setInput('fact', "Test fact");
+    favoritesServiceSpy.isFavorite.and.returnValue(true);
+    fixture.detectChanges();
+
+    const card = fixture.nativeElement.querySelector(".card");
+
+    expect(card.classList.contains("favorited")).toBeTrue();
   });
 });
